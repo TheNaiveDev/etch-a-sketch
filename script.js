a302d1fda2b65cfaf19ecf942e3f1558bcaf7dc0
Denzel Ashitey <131157495+TheNaiveDev@users.noreply.github.com>
2023-07-02 16:58:26 +0000

    Visual Representation Files

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.sub-container');
+const blackBtn = document.querySelector('.black-btn')
+const grayBtn = document.querySelector('.gray-btn')
+const clearBtn = document.querySelector('.clear-btn')
+const randomBtn = document.querySelector('.random-btn')
